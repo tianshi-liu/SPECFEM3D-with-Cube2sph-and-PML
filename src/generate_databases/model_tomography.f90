1149e912492d0e1cd5eefb4100e538cfbf613280
nqdu <nqdu@foxmail.com>
2025-07-01 22:04:00 -0400

    add -diag-disable=10441 for icc

--- src/generate_databases/model_tomography.f90
+++ src/generate_databases/model_tomography.f90
@@ -726,9 +726,6 @@ end subroutine init_tomography_files
 
   use generate_databases_par, only: ANISOTROPY
 
-  ! nqdu
-  use constants,only : CUBE2SPH_MESH
-
   implicit none
 
   double precision, intent(in) :: xmesh,ymesh,zmesh
@@ -766,10 +763,8 @@ end subroutine init_tomography_files
   real(kind=CUSTOM_REAL) :: qp_final,qs_final
   real(kind=CUSTOM_REAL) :: L_val
 
-  ! nqdu add for rotation
-  double precision :: d_temp(21),c_temp(21)
-  real(kind=CUSTOM_REAL) :: xp,yp,zp
-  real(kind=CUSTOM_REAL) :: r_dummy,theta,phi
+  ! nqdu add for voigt average
+  real(kind=CUSTOM_REAL) :: xA,xC,xL,xN
 
   ! initializes flag
   has_tomo_value = .false.
@@ -960,58 +955,13 @@ end subroutine init_tomography_files
     c66 = c_final(21)
 
     ! Voigt average
-    vp_model = sqrt(1./3.) * sqrt(0.5 * (c11 + c22) + 2. * c33)/sqrt(rho_model) 
-    vs_model = sqrt(1./3.) * sqrt(c66 + (c44+c55))/sqrt(rho_model) 
+    xA = (1.0 / 8.0) * (3.0 * c11 + 3.0 * c22 + 2.0 * c12 + 4.0 * c66);
+    xC = c33;
+    xN = (1.0 / 8.0) * (c11 + c22 - 2.0 * c12 + 4.0 * c66);
+    xL = 0.5 * (c44 + c55);
+    vp_model = sqrt(1./3. * (2. * xC + xA) / rho_model)
+    vs_model = sqrt(1./3. * (2. * xL + xN) / rho_model)
 
-    ! rotate to cartesian coordinates if required
-    if(CUBE2SPH_MESH) then 
-      ! set value 
-      xp = real(xmesh,kind=CUSTOM_REAL)
-      yp = real(ymesh,kind=CUSTOM_REAL)
-      zp = real(zmesh,kind=CUSTOM_REAL)
-      d_temp(:) = dble(c_final(:))
-      call xyz_2_rthetaphi(xp,yp,zp,r_dummy,theta,phi)
-      call rotate_tensor_radial_to_global(&
-        dble(theta),dble(phi),d_temp(1), d_temp(2), &
-        d_temp(3), d_temp(4), d_temp(5), d_temp(6), &
-        d_temp(7), d_temp(8), d_temp(9), d_temp(10), &  
-        d_temp(11), d_temp(12), d_temp(13), d_temp(14), &
-        d_temp(15), d_temp(16), d_temp(17), d_temp(18), &
-        d_temp(19), d_temp(20), d_temp(21), &
-        c_temp(1), c_temp(2), c_temp(3), c_temp(4),&
-        c_temp(5), c_temp(6), c_temp(7), c_temp(8), &
-        c_temp(9), c_temp(10), c_temp(11), c_temp(12), &
-        c_temp(13), c_temp(14), c_temp(15), c_temp(16),&
-        c_temp(17), c_temp(18), c_temp(19), c_temp(20), c_temp(21) &
-      )
-
-      ! copy c_temp to c_final
-      c_final(:) = real(c_temp(:),kind=CUSTOM_REAL)
-
-      ! copy back
-      c11 = c_final(1)
-      c12 = c_final(2)
-      c13 = c_final(3)
-      c14 = c_final(4)
-      c15 = c_final(5)
-      c16 = c_final(6)
-      c22 = c_final(7)
-      c23 = c_final(8)
-      c24 = c_final(9)
-      c25 = c_final(10)
-      c26 = c_final(11)
-      c33 = c_final(12)
-      c34 = c_final(13)
-      c35 = c_final(14)
-      c36 = c_final(15)
-      c44 = c_final(16)
-      c45 = c_final(17)
-      c46 = c_final(18)
-      c55 = c_final(19)
-      c56 = c_final(20)
-      c66 = c_final(21)
-    
-    endif
   else 
 
     ! Vp
