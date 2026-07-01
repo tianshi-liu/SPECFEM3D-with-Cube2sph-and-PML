10d94a065dd997066c8db6aed0908a82230297c5
nqdu <nqdu@foxmail.com>
2026-07-01 10:32:10 -0400

    update EXAMPLES

--- utils/cube2sph/EXAMPLES/cascadia/change_name.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for fname in ./*_27
-do
-	main_name="${fname%_27}"
-	cp ${main_name} ${main_name}_8
-	echo "cp ${main_name} ${main_name}_8"
-	cp ${main_name}_27 MESH-default/${main_name}
-	echo "cp ${main_name}_27 MESH-default/${main_name}"
-done
